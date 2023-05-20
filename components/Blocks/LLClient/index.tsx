c2228df75e2b444ecdb147f194840121362e402f
Pawel Lizner <pawel.lizner@gmail.com>
2023-05-20 18:14:55 +0200

    images

--- components/Blocks/LLClient/index.tsx
+++ components/Blocks/LLClient/index.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import Image from 'next/image';
+import React, { Fragment } from 'react';
 import { Media } from '../../Media';
 import { Media as MediaType } from '../../../payload-types';
 import { SectionContainer } from '../../SectionContainer';
@@ -20,10 +19,9 @@ export const LLClientBlock: React.FC<Props> = ({ clients }) => {
     <SectionContainer>
       <div className="my-10 md:my-20">
         <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
-      {clients.map((client) => <Media resource={client.media} className='rounded border p-4 flex items-center justify-center h-48' /> )}
+      {clients.map((client, i) => <Fragment key={i}><Media resource={client.media} className='rounded border p-4 flex items-center justify-center h-48' /></Fragment> )}
     </div>
       </div>
     </SectionContainer>
-
   )
 }
