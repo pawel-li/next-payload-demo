ccc6ed7bcb83ddf2070050b14bc824249629829e
Pawel Lizner <pawel.lizner@gmail.com>
2023-05-29 14:00:57 +0200

    title

--- app/head.tsx
+++ app/head.tsx
@@ -1,7 +1,7 @@
 export default function Head() {
   return (
     <>
-      <title></title>
+      <title>Lizner.pl</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <link rel="icon" href="/favicon.ico" />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
